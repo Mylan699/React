b7eda4615b90b28bc367bc12d70b338c0dd5e8c0
Mylan <mylan.mahieddine@ecole-isitech.fr>
2023-01-24 15:04:44 +0100

    fourth commit

--- src/components/ToDo.js
+++ src/components/ToDo.js
@@ -1,16 +1,24 @@
 import React, {useState} from 'react'
 import ToDoForm from './ToDoForm'
+import ToDoList from './ToDoList'
 
 function ToDo() {
     const [edit, setEdit] = useState({
         id: null,
         value: ''
     })
-    
 
-  return (
-    <div>ToDo</div>
-  )
+
+  return todos.map((todo, index) => (
+      <div className={todo.isComplete ? 'todo-row complete' : 'todo-row'}
+      key={index}>
+
+          <div key={todo.id} onClik={() => completeTodo(todo.id)}>
+              
+          </div>
+
+      </div>
+  ))
 }
 
 export default ToDo
