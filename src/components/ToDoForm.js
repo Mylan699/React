8f5b17c58839b6d47c13a9363ed2f0ddb1f3cff9
Mylan <mylan.mahieddine@ecole-isitech.fr>
2023-01-26 00:27:48 +0100

    end project

--- src/components/ToDoForm.js
+++ src/components/ToDoForm.js
@@ -1,18 +1,17 @@
-import React, { useState, useEffect, uesRef, useRef } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 
-function ToDoForm(props) {
-  const [input, setInput] = useState(props.edit ? props.edit.value
-    : '');
+function TodoForm(props) {
+  const [input, setInput] = useState(props.edit ? props.edit.value : '');
 
-  const inputRef = useRef(null)
+  const inputRef = useRef(null);
 
   useEffect(() => {
-    inputRef.current.focus()
-  })
+    inputRef.current.focus();
+  });
 
   const handleChange = e => {
-    setInput(e.target.value)
-  }
+    setInput(e.target.value);
+  };
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -21,24 +20,43 @@ function ToDoForm(props) {
       id: Math.floor(Math.random() * 10000),
       text: input
     });
-
-    setInput('')
-  }
+    setInput('');
+  };
 
   return (
-    <form className="todo-form" onSubmit={handleSubmit}>
-            
-      <input
-        type="text"
-        placeholder="Ecrire votre tache"
-        value={input} name="text"
-        className='todo-input'
-        onChange={handleChange}
-        ref={inputRef}
-      />
-      <button className="todo-button">Ajouter à faire </button>
-    </form>
-  )
+    <form onSubmit={handleSubmit} className='todo-form'>
+      {props.edit ? (
+        <>
+          <input
+            placeholder='Met a jour ta tâche '
+            value={input}
+            onChange={handleChange}
+            name='text'
+            ref={inputRef}
+            className='todo-input edit'
+          />
+          <button onClick={handleSubmit} className='todo-button edit'>
+            Mise à jour !
 
+          </button>
+        </>
+      ) : (
+        <>
+          <input
+            placeholder='Ajoute une tache'
+            value={input}
+            onChange={handleChange}
+            name='text'
+            className='todo-input'
+            ref={inputRef}
+          />
+          <button onClick={handleSubmit} className='todo-button'>
+            Ajout tâche !
+          </button>
+        </>
+      )}
+    </form>
+  );
 }
-export default ToDoForm
+
+export default TodoForm;
--- src/components/ToDoList.js
+++ src/components/ToDoList.js
@@ -1,35 +1,38 @@
-import React, { useState } from 'react'
-import ToDo from './ToDo';
-import ToDoForm from './ToDoForm'
+import React, { useState } from 'react';
+import TodoForm from './ToDoForm';
+import Todo from './ToDo';
 
-function ToDoList() {
+function TodoList() {
     const [todos, setTodos] = useState([]);
 
     const addTodo = todo => {
         if (!todo.text || /^\s*$/.test(todo.text)) {
-            return
+            return;
         }
 
-        const newTodos = [todo, ...todos]
-
+        const newTodos = [todo, ...todos];
+        //j'ai laissé volontairement un console.log ici car il ma été utile quand j'ai codé
         setTodos(newTodos);
-    }
+        console.log(...todos);
+    };
 
+    //test l'entrée utilisateur 
     const updateTodo = (todoId, newValue) => {
         if (!newValue.text || /^\s*$/.test(newValue.text)) {
-            return
+            return;
         }
 
-        setTodos(prev => prev.map(item =>(item.id === todoId ? newValue : item)))
-    }
+        setTodos(prev => prev.map(item => (item.id === todoId ? newValue : item)));
+    };
 
+    //déclaration de la variable pour suppression d'une tâche 
     const removeTodo = id => {
-        const removeArr = [...todos].filter(todo => todo.id !== id)
+        const removedArr = [...todos].filter(todo => todo.id !== id); //créer le tableau removeArr en récupérant les id sauf celui séléctionner
 
-        setTodos([...removeArr])
-    }
+        setTodos(removedArr);
+    };
 
-    
+    //déclaration de la variable pour validation d'une tâche 
     const completeTodo = id => {
         let updatedTodos = todos.map(todo => {
             if (todo.id === id) {
@@ -37,24 +40,22 @@ function ToDoList() {
             }
             return todo;
         });
-
         setTodos(updatedTodos);
+    };
 
-    }
-
+    //Titre afficher sur la page 
     return (
-        <div>
-            <h1>Qu'est ce que tu as à faire aujourd'hui ?</h1>
-            <ToDoForm onSubmit={addTodo} />
-            <ToDo todos={todos} completeTodo={completeTodo} removeTodo={removeTodo} 
-            updateTodo={updateTodo} />
-        </div>
-    )
-
-
+        <>
+            <h1>Qu'est ce que tu as à faire Aujourd'hui</h1>
+            <TodoForm onSubmit={addTodo} />
+            <Todo
+                todos={todos}
+                completeTodo={completeTodo}
+                removeTodo={removeTodo}
+                updateTodo={updateTodo}
+            />
+        </>
+    );
 }
 
-
-
-
-export default ToDoList
+export default TodoList;
